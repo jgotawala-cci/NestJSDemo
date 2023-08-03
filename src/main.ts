6584ac7bf89a3731b7ff002f7f6c8fb60176de3c
Jay Gotawala <jay.gotawala@creativecapsule.com>
2023-08-03 10:02:41 +0530

    Production evn added

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   // app.use(cookieSession({ keys: ['asdfasdf'] }));
-  await app.listen(process.env.PORT || 9001);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
